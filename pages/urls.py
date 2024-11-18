a7b895403010ed7b6ae9e091fe6842b39ee25083
petersch973 <peter@aco.co.nz>
2024-11-18 18:06:56 +1300

    3rd commit

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home_page_view, about_page_view
 
 urlpatterns = [
-    path("about/", about_page_view),
-    path("", home_page_view),
+    path("about/", about_page_view.as_view(),name = "about"),
+    path("", home_page_view, name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,18 @@
 
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
 # Create your views here.
 def home_page_view(request):
-    return HttpResponse("Homepage")
+    context = {"inventory_list": ["Widget 1", "Widget 2", "Widget 3"],"greeting": "THAnk you FOR visitING.",}
+    return render(request, "home.html", context)
 
-def about_page_view(request):
-    context = {
-        "name":"Peter",
-        "age": 67,
-        }
-    return render(request, "pages/about.html", context)
+class about_page_view(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "555-555-5555"
+        return context
+    
